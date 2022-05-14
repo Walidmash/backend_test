3be3f025939c95a3b6ca3c0f5a8b5bf89dfedd20
walidmash <waleedmash2@gmail.com>
2022-05-14 14:05:14 +0200

    test login endpoint

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -5,20 +5,46 @@ const app = require('../src/app');
 let testUser = {
   email: 'test5@test.com', password: 'test5'
 }
-test('user Login', t => {
+test('user Login success', t => {
   supertest(app)
     .post('/api/v1/login')
     .send({ email: testUser.email, password: testUser.password })
     .expect(200)
     .end((err, res) => {
-      if (err) {
-        t.notOk(err);
-      } else {
+      if (err) t.notOk(err);
+      else {
         t.same(res.body.email, testUser.email, `email should be the same as the input which is ${testUser.email}`);
         t.same(res.statusCode, 200, 'Status code is 200');
         testUser.token = res.body.token;
         t.end();
       }
-    });
+    })
+  });
+test('user Login missing password/email', t => {
+  supertest(app)
+    .post('/api/v1/login')
+    .send({ email: testUser.email })
+    .expect(400)
+    .end((err, res) => {
+      if (err) t.notOk(err);
+      else {
+        t.same(res.statusCode, 400, 'Status code is 400 in case password or email is not provided');
+        t.end();
+      }
+    })
 });
+test('user Login wrong password/email', t => {
+  supertest(app)
+    .post('/api/v1/login')
+    .send({ email: testUser.email, password: 'dsda' })
+    .expect(401)
+    .end((err, res) => {
+      if (err) t.notOk(err);
+      else {
+        t.same(res.statusCode, 401, 'Status code is 401 in case password or email is not correct');
+        t.end();
+      }
+    })
+});
+
 
